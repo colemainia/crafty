2c32c1509891c9edbe861f10348144334d692cda
Garrett Daly <garrettd333@gmail.com>
2017-12-20 19:36:26 -0700

    Adding in a small change to demonstrate a few things with PEP

--- tests/board_square.py
+++ tests/board_square.py
@@ -1,17 +1,30 @@
-# Chess board square
-# Coleman A. Hoyt
 class board_square():
-    # ctor: called by board, sets rank and file, possibly piece
-    def __init__(self, board_rank, board_file, piece = None):
-        self.board_rank = board_rank
-        self.board_file = board_file
-        self.piece = piece
+    """
+    This class encapsulates all attributes of a
+    single square in the board game 'Chess'.
+    """
+    def __init__(self, board_rank, board_file, piece=None):
+        self.board_rank = board_rank  # The number or row
+        self.board_file = board_file  # The column or letter
+        self.piece = piece  # Specific chess piece
+        self.dict_file_to_index = {
+            'a': 0,
+            'b': 1,
+            'c': 2,
+            'd': 3,
+            'e': 4,
+            'f': 5,
+            'g': 6,
+            'h': 7
+        }
 
-    def get_coordinates(self):
-        return (self.board_file, self.board_rank)
+    def translate_file(self, board_file):
+        """
+        Translate the file into a 0 based numeric.
 
-    def get_piece(self):
-        return self.piece
-
-    def set_piece(self, new_piece):
-        self.piece = new_piece
+        Args: board_file (char): a-h column character
+        Returns: file_index (int): 0-7 column integer
+        Raise: Can raise KeyError when board_file not in dict
+        """
+        file_index = self.dict_file_to_index[board_file]
+        return file_index
